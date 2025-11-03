633c6426572440f468f845860e0f95f4d6c7258b
thusitha bandara <tbandara@bne.catholic.edu.au>
2025-11-03 15:56:16 +1030

    updated project name

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,12 +18,12 @@ const config: Config = {
   url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/playQ-docs/',
+  baseUrl: '/playq-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'NcsQ', // Usually your GitHub org/user name.
-  projectName: 'playQ-docs', // Usually your repo name.
+  projectName: 'playq-docs', // Usually your repo name.
   deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
